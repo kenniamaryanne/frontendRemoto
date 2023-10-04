a191139cd0c44203ce7f30a356c584faf2beed08
kenniamaryanne <kenniamary2011a@gmail.com>
2023-10-03 22:06:56 -0300

    request to open camera added

--- src/app/entrada-foto01/entrada-foto01.component.ts
+++ src/app/entrada-foto01/entrada-foto01.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component,Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-entrada-foto01',
@@ -6,5 +6,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./entrada-foto01.component.css']
 })
 export class EntradaFoto01Component {
+  constructor(private renderer: Renderer2) { }
+
+  openCamera() {
+    const constraints: MediaStreamConstraints = {
+      video: { facingMode: 'user' } // Use a câmera frontal, se disponível
+    };
+  
+    navigator.mediaDevices
+      .getUserMedia(constraints)
+      .then((stream) => {
+        // Crie um elemento de vídeo para exibir a visualização da câmera
+        const videoElement = this.renderer.createElement('video');
+        videoElement.srcObject = stream;
+        videoElement.setAttribute('autoplay', 'true');
+  
+        // Adicione o elemento de vídeo ao DOM (por exemplo, a um modal)
+        const cameraModal = document.getElementById('camera-modal');
+        if (cameraModal) {
+          this.renderer.appendChild(cameraModal, videoElement);
+        }
+      })
+      .catch((error) => {
+        console.error('Erro ao acessar a câmera:', error);
+      });
+  }
+  
 
 }
